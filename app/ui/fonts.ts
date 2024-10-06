0d494a39d794d423656602cfea67963f3ff6add4
abdelkhaleksaadani <abdelkhaleksaadani@gmail.com>
2024-10-06 03:27:11 +0100

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana} from "next/font/google";
+
+// export const open_sans = Open_Sans({ subsets: ["latin"] }); 
+// export const afacad = Afacad({ subsets: ["latin"]});
+// export const roboto = Roboto({ subsets: ["latin"],weight: "400" });
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({ subsets: ["latin"], weight: "700" });
